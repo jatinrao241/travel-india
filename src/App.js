9dbc97d37c1081bba42102f86c80887c887ac3ef
jatinrao241 <jatinrao137@gmail.com>
2023-06-10 12:48:10 +0530

    url change of the website

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
     <>
       <Navbar />
       <Switch>
-        <Route path="/" component={Home} exact />
+        <Route path="/travel-india" component={Home} exact />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
         <Route path="/musicplaylist" component={MusicPlaylist} />
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav>
-            <NavLink exact activeClassName="active-css" className="custom-css-navbar" to='/'>Home</NavLink>
+            <NavLink exact activeClassName="active-css" className="custom-css-navbar" to='/travel-india'>Home</NavLink>
             <NavLink activeClassName="active-css" className="custom-css-navbar" to='/about'>About</NavLink>
             <NavLink activeClassName="active-css" className="custom-css-navbar" to='/contact'>Contact</NavLink>
             <NavLink activeClassName="active-css" className="custom-css-navbar" to='/musicplaylist'>Music</NavLink>
